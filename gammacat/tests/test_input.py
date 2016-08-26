481a02d3a3df677b4f964e91500b67769f821787
Christoph Deil <Deil.Christoph@gmail.com>
2016-08-26 21:37:56 +0200

    update

--- gammacat/tests/test_input.py
+++ gammacat/tests/test_input.py
@@ -2,14 +2,14 @@ import gammacat
 
 
 def test_basic_source_info():
-    info = gammacat.BasicSourceInfo.read('input/sources/tev-000074.yaml')
-    assert info.id == 'tev-000074'
+    info = gammacat.BasicSourceInfo.read('input/sources/tev-000083.yaml')
+    assert info.id == 83
 
 
 def test_paper_source_info():
-    info = gammacat.PaperSourceInfo.read('input/papers/2011A%26A...531L..18H/tev-000234.yaml')
+    info = gammacat.PaperSourceInfo.read('input/papers/2011A%26A...531L..18H/tev-000083.yaml')
     assert info.paper_id == '2011A&A...531L..18H'
-    assert info.source_id == 'tev-000234'
+    assert info.source_id == 83
 
 
 def test_paper_info():

689f9d4e22bfc9d3a059c43709ed359d3cbea4d7
Christoph Deil <Deil.Christoph@gmail.com>
2016-08-30 12:27:16 +0200

    update

--- gammacat/utils.py
+++ gammacat/utils.py
@@ -3,6 +3,7 @@ import json
 import yaml
 import numpy as np
 from astropy.table import Table
+from astropy.coordinates import SkyCoord
 
 MISSING_VAL = dict(
     integer=-999,
@@ -27,3 +28,17 @@ def make_table(row_data, column_def):
     a table with missing data.
     """
     pass
+
+
+def print_simbad_pos(name):
+    """Print YAML snipped for SIMBAD position.
+    """
+    pos = SkyCoord.from_name(name)
+    template = (
+        'pos:\n'
+        '  simbad_id: {name}\n'
+        '  ra: {pos.ra.deg}\n'
+        '  dec: {pos.dec.deg}\n'
+    )
+    s = template.format(name=name, pos=pos)
+    print(s)

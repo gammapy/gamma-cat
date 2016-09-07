cfb237b592f8e018d8c1349c6507b206bb9dcbf3
Christoph Deil <Deil.Christoph@gmail.com>
2016-09-07 20:25:03 +0200

    update

--- gammacat/utils.py
+++ gammacat/utils.py
@@ -1,4 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+from collections import OrderedDict
 from pathlib import Path
 import yaml
 import numpy as np
@@ -69,3 +70,45 @@ def rawgit_url(filename, location='master', mode='production'):
     url = '/'.join([base_url, location, filename])
 
     return url
+
+
+def yaml_make_ordereddict_work():
+    """
+    Teach YAML how to work with OrderedDict.
+
+    http://stackoverflow.com/a/21048064/498873
+    """
+    _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+
+    def dict_representer(dumper, data):
+        return dumper.represent_dict(data.items())
+
+    def dict_constructor(loader, node):
+        return OrderedDict(loader.construct_pairs(node))
+
+    yaml.add_representer(OrderedDict, dict_representer)
+    yaml.add_constructor(_mapping_tag, dict_constructor)
+
+
+def table_to_list_of_dict(table):
+    """Convert table to list of dict."""
+    rows = []
+    for row in table:
+        data = OrderedDict()
+        for name in table.colnames:
+            val = row[name]
+            if isinstance(val, np.int64):
+                val = int(val)
+            elif isinstance(val, np.bool_):
+                val = bool(val)
+            elif isinstance(val, np.float):
+                val = float(val)
+            elif isinstance(val, np.str):
+                val = str(val)
+            else:
+                raise ValueError('Unknown type: {} {}'.format(val, type(val)))
+            data[name] = val
+
+        rows.append(data)
+
+    return rows
--- other_cats/tgevcat/make.py
+++ other_cats/tgevcat/make.py
@@ -1,49 +1,7 @@
-import numpy as np
 import yaml
-from collections import OrderedDict
 from astropy.table import Table
 
-
-def yaml_make_ordereddict_work():
-    """
-    Teach YAML how to work with OrderedDict.
-
-    http://stackoverflow.com/a/21048064/498873
-    """
-    _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
-
-    def dict_representer(dumper, data):
-        return dumper.represent_dict(data.items())
-
-    def dict_constructor(loader, node):
-        return OrderedDict(loader.construct_pairs(node))
-
-    yaml.add_representer(OrderedDict, dict_representer)
-    yaml.add_constructor(_mapping_tag, dict_constructor)
-
-
-def table_to_list_of_dict(table):
-    """Convert table to list of dict."""
-    rows = []
-    for row in table:
-        data = OrderedDict()
-        for name in table.colnames:
-            val = row[name]
-            if isinstance(val, np.int64):
-                val = int(val)
-            elif isinstance(val, np.bool_):
-                val = bool(val)
-            elif isinstance(val, np.float):
-                val = float(val)
-            elif isinstance(val, np.str):
-                val = str(val)
-            else:
-                raise ValueError('Unknown type: {} {}'.format(val, type(val)))
-            data[name] = val
-
-        rows.append(data)
-
-    return rows
+from gammacat.utils import table_to_list_of_dict, yaml_make_ordereddict_work
 
 
 def main():

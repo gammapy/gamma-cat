2078f416abab73e47f77770f333c4e616e11e0e8
Matthias Wegen <wegenmat@nomos107.ifh.de>
2016-10-28 15:10:09 +0200

    modified DESY..ECSV.py so that source_id, paper_id and telescopes (multiple teescopes per paper possible) can be find in the header of the ECSV file. The Get_FITS..Archive.py script now contains the proper source-ids from gamma-cat

--- input/lightcurves/Get_FITS_from_DESY-LC-Archive.py
+++ input/lightcurves/Get_FITS_from_DESY-LC-Archive.py
@@ -8,7 +8,11 @@ import DESYLCArchiveToECSV
 ## links can be added if the DESY LC Archive gets extended
 ## source names can be easily edited and changed to tev-id for example
 
-url = {"1es1959+650":"http://www-zeuthen.desy.de/multi-messenger/GammaRayData/1es1959+650_combined_lc_v0.2.fits", "mrk421":"http://www-zeuthen.desy.de/multi-messenger/GammaRayData/mrk421_combined_lc_v0.2.fits", "mrk501":"http://www-zeuthen.desy.de/multi-messenger/GammaRayData/mrk501_combined_lc_v0.2.fits"}
+url = {
+    "tev-000138":"http://www-zeuthen.desy.de/multi-messenger/GammaRayData/1es1959+650_combined_lc_v0.2.fits", 
+    "tev-000049":"http://www-zeuthen.desy.de/multi-messenger/GammaRayData/mrk421_combined_lc_v0.2.fits", 
+    "tev-000091":"http://www-zeuthen.desy.de/multi-messenger/GammaRayData/mrk501_combined_lc_v0.2.fits"
+}
 
-for key, val in zip(url.keys(), url.values()):
+for key, val in url.items():
 	DESYLCArchiveToECSV.main(val, key)

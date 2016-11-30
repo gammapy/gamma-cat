ecdbcf275fc25351e1ad53d697b61dcf81456d2b
Christoph Deil <Deil.Christoph@gmail.com>
2016-11-30 12:25:17 +0100

    Add SED checks and processing to Python scripts

--- gammacat/sed.py
+++ gammacat/sed.py
@@ -26,9 +26,61 @@ class SED:
         table = Table.read(str(path), format=format)
         return cls(table=table, path=path)
 
+    def process(self):
+        """Apply fixes."""
+        table = self.table
+        self._process_energy_ranges(table)
+        self._process_flux_errrors(table)
+
+    @staticmethod
+    def _process_energy_ranges(table):
+        """
+        Sometimes energy bin ranges are given as `(e_lo, e_hi)`,
+        Those columns are not standard in the SED spec.
+        We convert those to `(e_min, e_max)`
+        """
+        if 'e_lo' in table.colnames:
+            table['e_min'] = table['e_ref'] - table['e_lo']
+            del table['e_lo']
+        if 'e_hi' in table.colnames:
+            table['e_max'] = table['e_ref'] + table['e_hi']
+            del table['e_hi']
+
+    @staticmethod
+    def _process_flux_errrors(table):
+        """
+        Sometimes flux errors are given as `(dnde_min, dnde_max)`,
+        i.e. 68% confidence level (1 sigma) limits.
+        Those columns are not standard in the SED spec.
+        We convert those to `dnde_errn` and `dnde_errp`.
+        """
+        if 'dnde_min' in table.colnames:
+            table['dnde_errn'] = table['dnde'] - table['dnde_min']
+            del table['dnde_min']
+        if 'dnde_max' in table.colnames:
+            table['dnde_errp'] = table['dnde_max'] - table['dnde']
+            del table['dnde_max']
+
     def validate(self):
         log.debug('Validating {}'.format(self.path))
         check_ecsv_column_header(self.path)
+        self.process()
+        self._validate_colnames()
+
+    def _validate_colnames(self):
+        expected_names = {
+            'e_ref', 'e_min', 'e_max',
+            'energy_lo', 'energy_hi',
+            'dnde', 'dnde_err', 'dnde_errn', 'dnde_errp', 'dnde_ul',
+            'excess', 'significance',
+        }
+        table = self.table
+        unexpected_colnames = set(table.colnames) - expected_names
+        if unexpected_colnames:
+            log.error(
+                'SED file {} contains invalid columns: {}'
+                ''.format(self.path, unexpected_colnames)
+            )
 
 
 class SEDList:
@@ -42,7 +94,6 @@ class SEDList:
         self.data = data
         _source_ids = [sed.table.meta['source_id'] for sed in data]
         self._sed_by_source_id = dict(zip(_source_ids, data))
-
 
     @classmethod
     def read(cls):

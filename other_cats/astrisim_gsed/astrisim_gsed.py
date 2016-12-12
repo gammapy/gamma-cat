c88ae46e2fc5cca24cb759abd8e44670655e6f47
Christoph Deil <Deil.Christoph@gmail.com>
2016-12-12 10:36:53 +0100

    Update other_cats/astrisim_gsed/astrisim_gsed.py

--- other_cats/astrisim_gsed/astrisim_gsed.py
+++ other_cats/astrisim_gsed/astrisim_gsed.py
@@ -5,12 +5,14 @@ Note: this script must be executed from the gamma-cat repo top-level folder.
 
 See `README.md`
 """
+from pathlib import Path
 from collections import OrderedDict
+from astropy.io import fits
 from astropy.table import Table
 import gammacat
 
 
-class GammaCatGSEDCheck:
+class AstriSimGSED:
     def __init__(self):
         self.gammacat_sources = gammacat.utils.load_json('docs/data/gammacat-sources.json')['data']
         self.gammacat_papers = gammacat.utils.load_json('docs/data/gammacat-papers.json')['data']
@@ -50,13 +52,37 @@ class GammaCatGSEDCheck:
                     paper_ids.append(source['paper_id'])
         return ','.join(paper_ids)
 
-    def add_missing_info(self):
-        """Add missing info from `astrisim/gsed to gamma-cat.
+    def dump_sed_to_ecsv(self):
+        """Dump SED info to ECSV files.
 
-        TODO: copy over info from FITS files to YAML / ECSV files,
-        in the appropritate formats.
+        We will then do the other steps of adding this data to gamma-cat manually,
+        i.e. find the right paper / folder / file to put the info and add info
+        to the header.
         """
-        raise NotImplementedError
+        base_path = Path('other_cats/astrisim_gsed/data/')
+        for path in base_path.glob('*.fits'):
+            source_name = path.parts[-1].replace('.fits', '')
+            hdu_list = fits.open(str(path))
+            hdu_names = [_.name for _ in hdu_list][1:]
+            hdu_names = [name for name in hdu_names if 'MAP' not in name]
+            for hdu_name in hdu_names:
+                # print(path, source_name, hdu_name)
+                table = Table.read(str(path), format='fits')
+                table.rename_column('ENERGY', 'e_ref')
+                table.rename_column('FLUX', 'dnde')
+                table.rename_column('FLUX_ERROR_MIN', 'dnde_errn')
+                table.rename_column('FLUX_ERROR_MAX', 'dnde_errp')
+
+                table.meta['SOURCE_NAME'] = source_name
+                try:
+                    del table.meta['HISTORY']
+                except:
+                    pass
+
+                filename = base_path / (source_name + '_' + hdu_name + '.ecsv')
+                print('Writing {}'.format(filename))
+                table.write(str(filename), format='ascii.ecsv', overwrite=True)
+                # import IPython; IPython.embed(); 1/0
 
     def check_consistency(self):
         """Check consistency of info in `astrisim/gsed to gamma-cat.
@@ -67,7 +93,7 @@ class GammaCatGSEDCheck:
 
 
 if __name__ == '__main__':
-    check = GammaCatGSEDCheck()
-    check.list_missing_info()
-    # check.add_missing_info()
+    gsed = AstriSimGSED()
+    gsed.list_missing_info()
+    gsed.dump_sed_to_ecsv()
     # check.check_consistency()

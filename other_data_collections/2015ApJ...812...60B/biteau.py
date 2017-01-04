86fb9f7d327cc91b08bc7640f053b7e562b563c3
Christoph Deil <Deil.Christoph@gmail.com>
2017-01-04 18:44:59 +0100

    Update other_data_collections/2015ApJ...812...60B

--- other_data_collections/2015ApJ...812...60B/biteau.py
+++ other_data_collections/2015ApJ...812...60B/biteau.py
@@ -6,13 +6,22 @@ from astropy.table import Table
 
 class Biteau:
     def __init__(self):
-        filename = 'other_data_collections/2015ApJ...812...60B/BiteauWilliams2015_AllData_ASDC_v2016_12_20.ecsv'
+        path = 'other_data_collections/2015ApJ...812...60B/'
+        filename = path + 'BiteauWilliams2015_AllData_ASDC_v2016_12_20.ecsv'
+        filename2 = path + 'BiteauWilliams2015_AllData_TeVCat_v2016_12_20.ecsv'
+
+        print('Reading {}'.format(filename))
         self.table = Table.read(filename, format='ascii.ecsv', delimiter='|')
 
+        print('Reading {}'.format(filename2))
+        self.table2 = Table.read(filename2, format='ascii.ecsv', delimiter='|')
+
     def run_checks(self):
-        # self.table.pprint()
-        self.table.show_in_browser(jsviewer=True)
         self.table.info('stats')
+        self.table2.info('stats')
+
+        self.table.show_in_browser(jsviewer=True)
+        self.table2.show_in_browser(jsviewer=True)
 
 
 if __name__ == '__main__':

888538a509786e34ed5cb3a6d6c2116bad8add4e
Christoph Deil <Deil.Christoph@gmail.com>
2017-01-04 22:27:52 +0100

    Small update to other_data_collections/2015ApJ...812...60B

--- other_data_collections/2015ApJ...812...60B/README.md
+++ other_data_collections/2015ApJ...812...60B/README.md
@@ -8,3 +8,18 @@ This folder contains spectral data for AGN from Biteau & Williams (2015)
 > I'd be happy if this dataset was accessible from your platform.
 > Please let me know if you have any question on the attached datafiles,
 > which list all the spectral points included in our study in two slightly different formats.
+
+The two data files are:
+* `BiteauWilliams2015_AllData_ASDC_v2016_12_20.ecsv`
+* `BiteauWilliams2015_AllData_TeVCat_v2016_12_20.ecsv`
+
+It looks like the content of the two files is basically the same:
+* 737 rows, i.e. spectral points
+* Extra columns `ra` and `dec` in `ASDC` irrelevant for us
+* Extra columns `freq_err` and `ul_flag` in `ASDC` file never filled
+
+These are the files Janathan sent, and Christoph Deil
+* wrote an ECSV header
+* fixed some data issues (see git commits for that file)
+
+From the `biteau.py` script, we're adding the data from the `ASDC` file to `gamma-cat`.
--- other_data_collections/2015ApJ...812...60B/biteau.py
+++ other_data_collections/2015ApJ...812...60B/biteau.py
@@ -4,7 +4,7 @@ Script to check and ingest Biteau & Williams (2015) data for gamma-cat.
 from astropy.table import Table
 
 
-class Biteau:
+class BiteauMaker:
     def __init__(self):
         path = 'other_data_collections/2015ApJ...812...60B/'
         filename = path + 'BiteauWilliams2015_AllData_ASDC_v2016_12_20.ecsv'
@@ -16,14 +16,21 @@ class Biteau:
         print('Reading {}'.format(filename2))
         self.table2 = Table.read(filename2, format='ascii.ecsv', delimiter='|')
 
-    def run_checks(self):
+    def run_basic_checks(self):
         self.table.info('stats')
         self.table2.info('stats')
 
         self.table.show_in_browser(jsviewer=True)
         self.table2.show_in_browser(jsviewer=True)
 
+    def run_checks(self):
+        # self.table.info('stats')
+        # self.table.pprint()
+        # ref_id = self.table['reference_id']
+        table = self.table.group_by('reference_id')
+        table.groups.keys.pprint(max_lines=-1)
 
 if __name__ == '__main__':
-    biteau = Biteau()
-    biteau.run_checks()
+    maker = BiteauMaker()
+    # biteau.run_basic_checks()
+    maker.run_checks()

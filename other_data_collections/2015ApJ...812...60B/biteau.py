5bf9de49c63bd77f03766572d4dbefebbbabaf48
Christoph Deil <Deil.Christoph@gmail.com>
2017-01-10 12:21:22 +0100

    Update other_data_collections/2015ApJ...812...60B/biteau.py

--- other_data_collections/2015ApJ...812...60B/biteau.py
+++ other_data_collections/2015ApJ...812...60B/biteau.py
@@ -2,6 +2,7 @@
 Script to check and ingest Biteau & Williams (2015) data for gamma-cat.
 """
 from astropy.table import Table
+import ads
 
 
 class BiteauMaker:
@@ -29,6 +30,15 @@ class BiteauMaker:
         # ref_id = self.table['reference_id']
         table = self.table.group_by('reference_id')
         table.groups.keys.pprint(max_lines=-1)
+
+        # Note: all references are valid ADS bibcodes now!
+        # reference_ids = list(table.groups.keys['reference_id'])
+        # for reference_id in reference_ids:
+        #     article = list(ads.SearchQuery(q='bibcode:{}'.format(reference_id)))[0]
+        #     print(reference_id, article)
+
+        # import IPython; IPython.embed()
+
 
 if __name__ == '__main__':
     maker = BiteauMaker()

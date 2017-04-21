7a7c39cdd9cbdfc777d93034c3a74b739b37bffc
Christoph Deil <Deil.Christoph@gmail.com>
2017-04-21 15:21:10 +0200

    update

--- gammacat/output.py
+++ gammacat/output.py
@@ -3,11 +3,11 @@
 Classes to read, validate and work with the input data files.
 """
 import logging
-from pathlib import Path
+from astropy.utils import lazyproperty
 from astropy.table import Table
 from .info import gammacat_info, gammacat_tag
 from .input import InputData
-from .utils import write_json
+from .utils import write_json, load_json
 
 __all__ = [
     'OutputDataConfig',
@@ -24,13 +24,13 @@ class OutputDataConfig:
     """
     path = gammacat_info.base_dir / 'docs/data'
 
+    gammacat_yaml = path / 'gammacat.yaml'  # TODO: generate from here!
     gammacat_ecsv = path / 'gammacat.ecsv'
     gammacat_fits = path / 'gammacat.fits.gz'
 
-    sources_json = path / 'gammacat-sources.json'
-
-    datasets_json = path / 'gammacat-datasets.json'
-    datasets_ecsv = path / 'gammacat-datasets.ecsv'
+    # Index files
+    index_datasets_json = path / 'gammacat-datasets.json'
+    index_sources_json = path / 'gammacat-sources.json'
 
     @staticmethod
     def make_filename(meta, datatype):
@@ -53,10 +53,12 @@ class OutputData:
     Expose it as Python objects that can be validated and used.
     """
 
-    def __init__(self, gammacat=None, datasets=None):
+    def __init__(self, index_dataset=None, index_sources=None,
+                 gammacat=None):
         self.path = OutputDataConfig.path
+        self.index_dataset = index_dataset
+        self.index_sources = index_sources
         self.gammacat = gammacat
-        self.datasets = datasets
 
     @classmethod
     def read(cls):
@@ -65,32 +67,21 @@ class OutputData:
         path = OutputDataConfig.gammacat_fits
         gammacat = Table.read(str(path), format='fits')
 
-        path = OutputDataConfig.datasets_ecsv
-        datasets = Table.read(str(path), format='ascii.ecsv')
+        index_dataset = load_json(OutputDataConfig.index_datasets_json)
+        index_sources = load_json(OutputDataConfig.index_sources_json)
 
         return cls(
+            index_dataset=index_dataset,
+            index_sources=index_sources,
             gammacat=gammacat,
-            datasets=datasets,
         )
 
     def __str__(self):
         ss = 'Output data summary:\n'
         ss += 'Path: {}\n'.format(self.path)
         ss += 'Number of sources: {}\n'.format(len(self.gammacat))
-        ss += 'Number of datasets: {}\n'.format(len(self.datasets))
+        ss += 'Number of datasets: {}\n'.format(len(self.index_dataset['data']))
         return ss
-
-        # ss += 'Number of YAML files in `input/sources`: {}\n'.format(len(self.sources.data))
-        # ss += 'Number of entries in `input/gammacat/gamma_cat_dataset.yaml`: {}\n'.format(
-        #     len(self.gammacat_dataset_config.data))
-        # ss += '\n'
-        # ss += 'Number of folders in `input/data`: {}\n'.format(len(self.datasets.data))
-        # ss += 'Number of total datasets in `input/gammacat/gamma_cat_dataset.yaml`: {}\n'.format(
-        #     len(self.gammacat_dataset_config.reference_ids))
-        # ss += '\n'
-        # ss += 'Number of SEDs: {}\n'.format(len(self.seds.data))
-        # ss += 'Number of lightcurves: {}\n'.format(len(self.lightcurves.data))
-        # return ss
 
     def validate(self):
         log.info('Validating output data ...')
@@ -112,39 +103,35 @@ class OutputDataMaker:
     """
 
     def __init__(self):
-        self.input_data = InputData.read()
+        pass
+
+    @lazyproperty
+    def input_data(self):
+        log.info('Reading input data ...')
+        return InputData.read()
 
     def make_all(self):
         self.make_sed_files()
         self.make_index_files()
 
     def make_index_files(self):
-        self.make_source_table_json()
-        self.make_dataset_table_json()
-        self.make_dataset_table_ecsv()
+        self.make_index_files_datasets()
+        self.make_index_files_sources()
 
-    def make_source_table_json(self):
-        data = self.input_data.sources.to_json()
-        path = OutputDataConfig.sources_json
-        write_json(data, path)
-
-    def make_dataset_table_json(self):
+    def make_index_files_datasets(self):
         data = self.input_data.datasets.to_json()
-        path = OutputDataConfig.datasets_json
+        path = OutputDataConfig.index_datasets_json
         write_json(data, path)
 
-    def make_dataset_table_ecsv(self):
-        table = self.input_data.datasets.to_table()
-        path = OutputDataConfig.datasets_ecsv
-        log.info('Writing {}'.format(path))
-        table.write(str(path), format='ascii.ecsv')
+    def make_index_files_sources(self):
+        data = self.input_data.sources.to_json()
+        path = OutputDataConfig.index_sources_json
+        write_json(data, path)
 
     def make_sed_files(self):
         for sed in self.input_data.seds.data:
             log.debug('Processing SED: {}'.format(sed.path))
-
             sed.process()
-
             path = OutputDataConfig.make_filename(meta=sed.table.meta, datatype='sed')
             path.parent.mkdir(parents=True, exist_ok=True)
             log.info('Writing {}'.format(path))

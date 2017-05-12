bd4dcf6a337dee90288bbe71dc5bda5ee024383d
Peter Deiml <peter.deiml@fau.de>
2017-05-12 15:02:53 +0200

    Add PKS 1510-089, PG 1553+113 from Biteau Catalog

--- other_data_collections/2015ApJ...812...60B/add_biteau.py
+++ other_data_collections/2015ApJ...812...60B/add_biteau.py
@@ -19,12 +19,12 @@ biteau.adapt_source_names(table)
 
 # new_table.write('Relevant_Biteau_Data.ecsv', format='ascii.ecsv', delimiter=' ')
 
-# filecounter = 2
-# note = 'High'
+# filecounter = 3
+# note = '2009'
 experiment = 'HESS'
-reference_id = '2013A&A...552A.118H'
-source = 'PKS 0447-439'
-source_id = '000021'
+reference_id = '2015ApJ...802...65A'
+source = 'PG 1553+113'
+source_id = '000082'
 
 # biteau.create_escv1(table, filecounter, note, experiment, reference_id, source, source_id)
 biteau.create_escv2(table, experiment, reference_id, source, source_id)

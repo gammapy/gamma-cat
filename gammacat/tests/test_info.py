09ef0f8b1ee8153c363713191331308974c89e67
Christoph Deil <Deil.Christoph@gmail.com>
2018-02-16 01:05:53 +0100

    Fix gammacat/tests/test_info.py

--- gammacat/tests/test_info.py
+++ gammacat/tests/test_info.py
@@ -6,4 +6,4 @@ def test_gammacat_str():
     assert GammaCatStr.source_id_str(42) == '000042'
 
     meta = dict(source_id=42, reference_id='2015A&A...577A.131H')
-    assert GammaCatStr.dataset_filename(meta) == 'gammacat_2015A%26A...577A.131H_000042'
+    assert GammaCatStr.data_filename(meta) == '2015A%26A...577A.131H/tev-000042'
